3a60b00ad1e05b5d215679d75169d28442a98448
fsarrias <fsarrias@kolab.es>
2020-04-22 11:49:19 +0200

    subo la fase1 corregida

--- src/fase1.java
+++ src/fase1.java
@@ -1,33 +1,33 @@
 import java.util.Scanner;
 
 public class fase1 {
-    public String city1;
-    public String city2;
-    public String city3;
-    public String city4;
-    public String city5;
-    public String city6;
-
     public static void main(String[] args) {
+        String city1;
+        String city2;
+        String city3;
+        String city4;
+        String city5;
+        String city6;
+
         Scanner entrada = new Scanner(System. in);
 
         System.out.print("Enter the name of a city : ");
-        String city1 = entrada. nextLine();
+        city1 = entrada. nextLine();
 
         System.out.print("Enter the name of a city : ");
-        String city2 = entrada. nextLine();
+        city2 = entrada. nextLine();
 
         System.out.print("Enter the name of a city : ");
-        String city3 = entrada. nextLine();
+        city3 = entrada. nextLine();
 
         System.out.print("Enter the name of a city : ");
-        String city4 = entrada. nextLine();
+        city4 = entrada. nextLine();
 
         System.out.print("Enter the name of a city : ");
-        String city5 = entrada. nextLine();
+        city5 = entrada. nextLine();
 
         System.out.print("Enter the name of a city : ");
-        String city6 = entrada. nextLine();
+        city6 = entrada. nextLine();
 
         System.out.println("The name of the cities are : \n"+city1+", "+city2+", "+city3+", "+city4+", "+city5+", "+city6);
     }
